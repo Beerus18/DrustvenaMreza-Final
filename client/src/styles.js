ca0b1492977e2082fae6109d69d2f71de0a5d753
vanjavii <vanjolinav@gmail.com>
2021-05-19 12:13:28 +0200

    Finalne izmene

--- client/src/styles.js
+++ client/src/styles.js
@@ -15,7 +15,7 @@ export default makeStyles((theme) => ({
     alignItems: 'center',
   },
   heading: {
-    color: 'rgba(0,183,255, 1)',
+    color: "#48D1CC",
   },
   image: {
     marginLeft: '15px',
